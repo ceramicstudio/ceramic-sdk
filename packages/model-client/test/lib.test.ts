1324e07b88feba392dc40f0fda47eb4e7deb28fc
Paul Le Cam <paul@3box.io>
2024-08-02 15:38:17 +0100

    [PR 5/9] Model protocol and client (#11)

---
+++ packages/model-client/test/lib.test.ts
@@ -0,0 +1,71 @@
+import { EthereumDID } from '@ceramic-sdk/ethereum-did'
+import { assertSignedEvent } from '@ceramic-sdk/events'
+import { getAuthenticatedDID } from '@ceramic-sdk/key-did'
+import {
+  MODEL_RESOURCE_URI,
+  type ModelDefinition,
+} from '@ceramic-sdk/model-protocol'
+
+import { createInitEvent } from '../src/index.js'
+
+const authenticatedDID = await getAuthenticatedDID(new Uint8Array(32))
+
+const testModelV1: ModelDefinition = {
+  version: '1.0',
+  name: 'TestModel',
+  description: 'Test model',
+  accountRelation: { type: 'list' },
+  schema: {
+    type: 'object',
+    properties: {
+      test: { type: 'string', maxLength: 10 },
+    },
+    additionalProperties: false,
+  },
+}
+
+describe('createInitEvent()', () => {
+  test('validates the content', async () => {
+    // @ts-expect-error
+    const invalidDefinition: ModelDefinition = { ...testModelV1, version: '0' }
+    await expect(async () => {
+      await createInitEvent(authenticatedDID, invalidDefinition)
+    }).rejects.toThrow('Unsupported version format: 0')
+  })
+
+  describe('validates the controller', () => {
+    test('supports did:key', async () => {
+      expect(authenticatedDID.id.startsWith('did:key')).toBe(true)
+      await expect(
+        createInitEvent(authenticatedDID, testModelV1),
+      ).resolves.not.toThrow()
+    })
+
+    test('supports did:pkh', async () => {
+      const did = await EthereumDID.random({
+        domain: 'test',
+        resources: [MODEL_RESOURCE_URI],
+      })
+
+      const [validSession, invalidSession] = await Promise.all([
+        did.createSession({ expirationTime: null }),
+        did.createSession({ expiresInSecs: 60 }),
+      ])
+
+      await expect(async () => {
+        await createInitEvent(invalidSession.did, testModelV1)
+      }).rejects.toThrow(
+        'Invalid CACAO: Model Streams do not support CACAOs with expiration times',
+      )
+
+      await expect(
+        createInitEvent(validSession.did, testModelV1),
+      ).resolves.not.toThrow()
+    })
+  })
+
+  test('returns the signed event', async () => {
+    const event = await createInitEvent(authenticatedDID, testModelV1)
+    assertSignedEvent(event)
+  })
+})
