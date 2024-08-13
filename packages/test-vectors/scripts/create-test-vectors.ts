50f8fbcc529cec2db1899fdfcd31b9aad314ac76
Paul Le Cam <paul@3box.io>
2024-08-13 16:00:37 +0100

    Add Ethereum DID to test vectors

--- packages/test-vectors/scripts/create-test-vectors.ts
+++ packages/test-vectors/scripts/create-test-vectors.ts
@@ -10,7 +10,9 @@ import {
   getDeterministicInitEvent,
 } from '@ceramic-sdk/model-instance-client'
 import { getStreamID } from '@ceramic-sdk/model-instance-protocol'
+import { EthereumDID, type Hex } from '@ceramic-sdk/test-utils'
 import { getAuthenticatedDID } from '@didtools/key-did'
+import type { DID } from 'dids'
 
 import { createCAR } from './utils/car.ts'
 import { createControllerDir, writeCARFile } from './utils/fs.ts'
@@ -19,41 +21,73 @@ const model = StreamID.fromString(
   'k2t6wz4z9kggqqnbn3vqggh2z4fe9jctr9vvbc1em2yho0qnzzrtzz1n2hr4lq',
 )
 
-const controller = await getAuthenticatedDID(new Uint8Array(32))
+type Controller = {
+  id: string
+  signer: DID
+}
 
-const path = await createControllerDir('key-plain', {
-  controller: controller.id,
-  model: model.toString(),
-})
+const controllerFactories = {
+  'key-plain': async () => {
+    const did = await getAuthenticatedDID(new Uint8Array(32))
+    return { id: did.id, signer: did }
+  },
+  'pkh-ethereum': async () => {
+    const ethereumDID = await EthereumDID.fromPrivateKey(
+      '0xe50df915de22bad5bf1abf43f78b55d64640afdcdfa6b1699a514d97662b23f7' as Hex,
+      { domain: 'localhost', resources: ['ceramic://*'] },
+    )
+    const session = await ethereumDID.createSession()
+    return { id: ethereumDID.id, signer: session.did }
+  },
+} satisfies Record<string, () => Promise<Controller>>
 
-const deterministicEvent = getDeterministicInitEvent(model, controller.id)
+for (const [controllerType, createController] of Object.entries(
+  controllerFactories,
+)) {
+  // Setup
 
-await writeCARFile(
-  [path, 'valid', 'deterministic'],
-  createCAR(deterministicEvent),
-)
-
-const initPayload = InitEventPayload.encode({
-  data: { test: true },
-  header: createInitHeader({
+  const controller = await createController()
+  const dirPath = await createControllerDir(controllerType, {
     controller: controller.id,
-    model,
-    unique: new Uint8Array([0, 1, 2, 3]),
-  }),
-})
-await writeCARFile([path, 'valid', 'init-payload'], createCAR(initPayload))
+    model: model.toString(),
+  })
 
-const initEvent = await signEvent(controller, initPayload)
-const initCAR = signedEventToCAR(initEvent)
-const initCID = initCAR.roots[0]
-await writeCARFile([path, 'valid', 'init-event'], initCAR)
+  // Deterministic (init) event
 
-const streamID = getStreamID(initCID)
+  const deterministicEvent = getDeterministicInitEvent(model, controller.id)
+  await writeCARFile(
+    [dirPath, 'valid', 'deterministic'],
+    createCAR(deterministicEvent),
+  )
 
-const dataPayload = createDataEventPayload(CommitID.fromStream(streamID), [
-  { op: 'add', path: '/update', value: true },
-])
-await writeCARFile([path, 'valid', 'data-payload'], createCAR(dataPayload))
+  // Signed init event
 
-const dataEvent = await signEvent(controller, dataPayload)
-await writeCARFile([path, 'valid', 'data-event'], signedEventToCAR(dataEvent))
+  const initPayload = InitEventPayload.encode({
+    data: { test: true },
+    header: createInitHeader({
+      controller: controller.id,
+      model,
+      unique: new Uint8Array([0, 1, 2, 3]),
+    }),
+  })
+  await writeCARFile([dirPath, 'valid', 'init-payload'], createCAR(initPayload))
+
+  const initEvent = await signEvent(controller.signer, initPayload)
+  const initCAR = signedEventToCAR(initEvent)
+  const initCID = initCAR.roots[0]
+  await writeCARFile([dirPath, 'valid', 'init-event'], initCAR)
+
+  // Data event
+
+  const streamID = getStreamID(initCID)
+  const dataPayload = createDataEventPayload(CommitID.fromStream(streamID), [
+    { op: 'add', path: '/update', value: true },
+  ])
+  await writeCARFile([dirPath, 'valid', 'data-payload'], createCAR(dataPayload))
+
+  const dataEvent = await signEvent(controller.signer, dataPayload)
+  await writeCARFile(
+    [dirPath, 'valid', 'data-event'],
+    signedEventToCAR(dataEvent),
+  )
+}
